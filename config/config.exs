9c783714ac73487c18bf49cf54ee81ad8e50ab7c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-11-13 00:33:02 +0100

    Implemet proper application

--- config/config.exs
+++ config/config.exs
@@ -2,3 +2,7 @@ import Config
 
 config :logger, :console, format: "$time [$level] $metadata $levelpad$message\n"
 config :tesla, Tesla.Middleware.Logger, debug: false
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
--- lib/elixir_syncthing_bot.ex
+++ lib/elixir_syncthing_bot.ex
@@ -1,8 +1,37 @@
 defmodule ElixirSyncthingBot do
   use Application
 
+  alias ElixirSyncthingBot.Notifiers.FoldersState
+  alias ElixirSyncthingBot.Notifiers.Notifier
+
+  alias ElixirSyncthingBot.Syncthing.Api.ConfigListener
+  alias ElixirSyncthingBot.Syncthing.Api.EventListener
+
   def start(_type, _args) do
-    children = []
-    Supervisor.start_link(children, strategy: :one_for_one)
+    [notifier: notifier, notifier_options: notifier_options] =
+      Application.get_env(:elixir_syncthing_bot, :notifier)
+
+    children = [
+      {Registry, [keys: :unique, name: Registry.ElixirSyncthingBot]},
+      FoldersState,
+      Notifier.notifier(notifier, notifier_options)
+    ]
+
+    [servers: servers] = Application.get_env(:elixir_syncthing_bot, :syncthing, :servers)
+
+    servers =
+      servers
+      |> String.split(";")
+      |> Enum.map(&URI.parse/1)
+      |> Enum.map(fn uri ->
+        [host: "#{uri.scheme}://#{uri.host}:#{uri.port}/#{uri.query}", token: uri.userinfo]
+      end)
+      |> Enum.flat_map(fn server ->
+        [{ConfigListener, server}, {EventListener, server}]
+      end)
+
+    opts = [strategy: :one_for_one, name: ElixirSyncthingBot.Supervisor]
+
+    Supervisor.start_link(children ++ servers, opts)
   end
 end
