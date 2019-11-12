1e85914b1baf7f329533ad747b559ce66d95d77d
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-11-12 23:50:31 +0100

    Revert "Handle a case when user does not have a wallet"
    This reverts commit de2cb2fb89e33d76a503b5c39f7f04149f387eeb.

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,4 @@
 import Config
 
-config :logger, :console, format: {ElixirSyncthingBot.ColorLogger, :format}
+config :logger, :console, format: "$time [$level] $metadata $levelpad$message\n"
 config :tesla, Tesla.Middleware.Logger, debug: false
