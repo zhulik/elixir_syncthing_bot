ef5a5e9d3c4ec139c1739fbb45d601b50f613c02
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-11-14 21:40:58 +0100

    Fix config loading

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,6 @@ import Config
 config :logger, :console, format: "$time [$level] $metadata $levelpad$message\n"
 config :tesla, Tesla.Middleware.Logger, debug: false
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+if File.exists?("#{Mix.env()}.exs") do
+  import_config "#{Mix.env()}.exs"
+end
