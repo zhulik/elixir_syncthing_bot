5a00af22835728b5321a61e334815c7f08e29233
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2019-12-19 21:23:08 +0100

    Remove ServersSupervisor

--- lib/elixir_syncthing_bot.ex
+++ lib/elixir_syncthing_bot.ex
@@ -6,7 +6,9 @@ defmodule ElixirSyncthingBot do
   alias ElixirSyncthingBot.Notifiers.FoldersState
   alias ElixirSyncthingBot.Notifiers.NotifierDispatcher
 
-  alias ElixirSyncthingBot.ServersSupervisor
+  alias ElixirSyncthingBot.Syncthing.Api.ConfigListener
+  alias ElixirSyncthingBot.Syncthing.Api.ConnectionsListener
+  alias ElixirSyncthingBot.Syncthing.Api.EventListener
 
   def start(_type, _args) do
     notifier = System.get_env("NOTIFIER") || "console"
@@ -15,30 +17,40 @@ defmodule ElixirSyncthingBot do
     children = [
       {Registry, [keys: :unique, name: Registry.ElixirSyncthingBot]},
       FoldersState,
-      NotifierDispatcher.notifier(notifier, notifier_options),
-      ServersSupervisor
+      NotifierDispatcher.notifier(notifier, notifier_options)
     ]
 
-    opts = [strategy: :one_for_one, name: ElixirSyncthingBot.Supervisor]
-
-    res = Supervisor.start_link(children, opts)
-
-    (System.get_env("SERVERS") || "")
-    |> String.trim()
-    |> String.split(";")
-    |> Enum.map(&String.trim/1)
-    |> Enum.filter(fn server -> server != "" end)
-    |> Enum.map(&URI.parse/1)
-    |> Enum.map(fn uri ->
-      Api.client(
-        host: "#{uri.scheme}://#{uri.host}:#{uri.port}#{uri.path}",
-        token: uri.userinfo
-      )
-    end)
-    |> Enum.each(&ServersSupervisor.add_server/1)
+    servers =
+      (System.get_env("SERVERS") || "")
+      |> String.trim()
+      |> String.split(";")
+      |> Enum.map(&String.trim/1)
+      |> Enum.filter(fn server -> server != "" end)
+      |> Enum.map(&URI.parse/1)
+      |> Enum.map(fn uri ->
+        Api.client(
+          host: "#{uri.scheme}://#{uri.host}:#{uri.port}#{uri.path}",
+          token: uri.userinfo
+        )
+      end)
+      |> Enum.map(&server_specs/1)
 
     {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
 
-    res
+    opts = [strategy: :one_for_one, name: ElixirSyncthingBot.Supervisor]
+    Supervisor.start_link(children ++ servers, opts)
+  end
+
+  defp server_specs(api) do
+    children = [
+      {EventListener, api},
+      {ConfigListener, api},
+      {ConnectionsListener, api}
+    ]
+
+    %{
+      id: "#{api.host}.supervisor",
+      start: {Supervisor, :start_link, [children, [strategy: :one_for_one]]}
+    }
   end
 end
